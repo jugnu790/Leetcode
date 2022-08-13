0387ca7600249e903e8cead7f7f31f9ee694139b
DEEPESH UPADHYAY <87692640+jugnu790@users.noreply.github.com>
2022-08-13 22:42:33 +0530

    Time: 303 ms (34.68%), Space: 112.9 MB (32.05%) - LeetHub

--- 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.java
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.java
@@ -1,26 +1,35 @@
-
-class Solution {
+public class Solution {
     public List<Integer> findSubstring(String s, String[] words) {
-        List<Integer> res = new ArrayList<>();
-        if(s.length() == 0 || words.length == 0) return res;
-        int wordLen = words[0].length();
-        int totalLen = wordLen * words.length;
-        for(int i = 0; i < s.length() - totalLen + 1; i++) {
-            String cur = s.substring(i, i + totalLen);
-            if(isValid(cur, words, wordLen)) res.add(i);
+        LinkedList<Integer> res = new LinkedList<>();
+        int n = words.length; // the number words
+        int l = words[0].length(); // the length of each word
+        HashMap<String, Integer> map = new HashMap<>();
+        for (String word : words) { // build map
+            int val = 1;
+            if (map.containsKey(word)) {
+                val += map.get(word);
+            }
+            map.put(word, val);
+        }
+        for (int i = 0; i + n * l - 1 < s.length(); i++) {
+            HashMap<String, Integer> tmp = (HashMap<String, Integer>) map.clone();
+            for (int j = 0; j < n; j++) {
+                String str = s.substring(i + j * l, i + (j + 1) * l);
+                if (tmp.containsKey(str)) {
+                    int val = tmp.get(str);
+                    if (val > 1) {
+                        tmp.put(str, val - 1);
+                    } else {
+                        tmp.remove(str);
+                    }
+                } else {
+                    break;
+                }
+            }
+            if (tmp.size() == 0) {
+                res.add(i);
+            }
         }
         return res;
-    }
-    private boolean isValid(String cur, String[] words, int wordLen) {
-        Map<String, Integer> map = new HashMap<>();
-        for(String word : words) map.put(word, map.getOrDefault(word, 0) + 1);
-        Map<String, Integer> curMap = new HashMap<>();
-        for(int i = 0; i < cur.length(); i += wordLen) {
-            String word = cur.substring(i, i + wordLen);
-            if(!map.containsKey(word)) return false;
-            curMap.put(word, curMap.getOrDefault(word, 0) + 1);
-            if(curMap.get(word) > map.get(word)) return false;
-        }
-        return true;
     }
 }
