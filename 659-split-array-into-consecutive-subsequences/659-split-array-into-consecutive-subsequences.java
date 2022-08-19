f46899cad3d011ceb455cbd73c12db78c6cd806e
DEEPESH UPADHYAY <87692640+jugnu790@users.noreply.github.com>
2022-08-19 13:17:23 +0530

    Time: 257 ms (5.23%), Space: 66.7 MB (82.95%) - LeetHub

--- 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.java
+++ 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.java
@@ -1,27 +1,146 @@
 class Solution {
     public boolean isPossible(int[] nums) {
-        PriorityQueue<interval> queue = new PriorityQueue<>((a, b) -> (a.end == b.end ? a.len - b.len : a.end - b.end));
-        for (int num : nums) {
-            while (!queue.isEmpty() && queue.peek().end + 1 < num) {
-                if (queue.poll().len < 3) return false;
-            }
-            if (queue.isEmpty() || queue.peek().end == num) queue.offer(new interval(num, num));
-            else { //num == end + 1
-                queue.add(new interval(queue.poll().start, num));
-            }
-        }
-        while (!queue.isEmpty()) {
-            if (queue.poll().len < 3) return false;
-        }
-        return true;
-    }
-    
-    class interval {
-        int start, end, len;
-        public interval(int start, int end) {
-            this.start = start;
-            this.end = end;
-            this.len = end - start + 1;
-        }
-    }
+
+
+      if (nums == null || nums.length < 3) {
+
+
+          return false;
+
+
+      }
+
+
+      List<List<Integer>> subsequences = new ArrayList<>();
+
+
+      for (int num : nums) {
+
+
+          if (subsequences.isEmpty()) {
+
+
+              List<Integer> subsequence = new ArrayList<>();
+
+
+              subsequence.add(num);
+
+
+              subsequences.add(subsequence);
+
+
+          } else {
+
+
+              boolean inserted = false;
+
+
+              List<Integer> firstSubsequence = null;
+
+
+              for (List<Integer> subsequence : subsequences) {
+
+
+                  if (subsequence.get(subsequence.size() - 1) == num - 1) {
+
+
+                      if(null == firstSubsequence){
+
+
+                          firstSubsequence = subsequence;
+
+
+                      }
+
+
+                      if(subsequence.size() < 3) {
+
+
+                          subsequence.add(num);
+
+
+                          inserted = true;
+
+
+                          break;
+
+
+                      }
+
+
+                  }
+
+
+
+
+                  if (subsequence.get(subsequence.size() - 1) < num - 1 && subsequence.size() < 3){
+
+
+                      return false;
+
+
+                  }
+
+
+              }
+
+
+
+
+              if (!inserted && firstSubsequence != null) {
+
+
+                  firstSubsequence.add(num);
+
+
+                  inserted = true;
+
+
+              }
+
+
+
+
+              if (!inserted) {
+
+
+                  List<Integer> subsequence = new ArrayList<>();
+
+
+                  subsequence.add(num);
+
+
+                  subsequences.add(subsequence);
+
+
+              }
+
+
+          }
+
+
+      }
+
+
+
+
+      for (List<Integer> subsequence : subsequences) {
+
+
+          if (subsequence.size() < 3) {
+
+
+              return false;
+
+
+          }
+
+
+      }
+
+
+      return true;
+
+
+  }
 }
