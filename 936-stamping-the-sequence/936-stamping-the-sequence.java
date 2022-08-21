b656d91e4f943485f837ba5935c62c8d03a85f45
DEEPESH UPADHYAY <87692640+jugnu790@users.noreply.github.com>
2022-08-21 23:47:01 +0530

    Time: 7 ms (71.43%), Space: 43.9 MB (86.61%) - LeetHub

--- 936-stamping-the-sequence/936-stamping-the-sequence.java
+++ 936-stamping-the-sequence/936-stamping-the-sequence.java
@@ -1,62 +1,25 @@
 class Solution {
-    public int[] movesToStamp(String stamp, String target) {
-        int stampLength = stamp.length(), targetLength = target.length();
-        boolean[] finished = new boolean[targetLength];
-        Queue<Integer> queue = new LinkedList<Integer>();
-        Stack<Integer> stack = new Stack<Integer>();
-        List<Set<Integer>> matchList = new ArrayList<Set<Integer>>();
-        List<Set<Integer>> notMatchList = new ArrayList<Set<Integer>>();
-        int maxIndex = targetLength - stampLength;
-        for (int i = 0; i <= maxIndex; i++) {
-            Set<Integer> matchSet = new HashSet<Integer>();
-            Set<Integer> notMatchSet = new HashSet<Integer>();
-            for (int j = 0; j < stampLength; j++) {
-                if (stamp.charAt(j) == target.charAt(i + j))
-                    matchSet.add(i + j);
-                else
-                    notMatchSet.add(i + j);
-            }
-            matchList.add(matchSet);
-            notMatchList.add(notMatchSet);
-            if (notMatchSet.isEmpty()) {
-                stack.push(i);
-                for (int j = 0; j < stampLength; j++) {
-                    int index = i + j;
-                    if (!finished[index]) {
-                        finished[index] = true;
-                        queue.offer(index);
-                    }
+    public int[] movesToStamp(String S, String T) {
+        if (S == T) return new int[]{0};
+        char[] SC = S.toCharArray(), TC = T.toCharArray();
+        int slen = SC.length, tlen = TC.length - slen + 1, i, j;
+        List<Integer> lans = new ArrayList<>();
+        Boolean tdiff = true, sdiff;
+        while (tdiff)
+            for (i = 0, tdiff = false; i < tlen; i++) {
+                for (j = 0, sdiff = false; j < slen; j++)
+                    if (TC[i+j] == '*') continue;
+                    else if (TC[i+j] != SC[j]) break;
+                    else sdiff = true;
+                if (j == slen && sdiff) {
+                    for (j = i, tdiff = true; j < slen + i; j++)
+                        TC[j] = '*';
+                    lans.add(0, i);
                 }
             }
-        }
-        while (!queue.isEmpty()) {
-            int index = queue.poll();
-            int startIndex = Math.max(0, index + 1 - stampLength);
-            int endIndex = Math.min(maxIndex, index);
-            for (int i = startIndex; i <= endIndex; i++) {
-                Set<Integer> notMatchSet = notMatchList.get(i);
-                if (notMatchSet.remove(index)) {
-                    if (notMatchSet.isEmpty()) {
-                        stack.push(i);
-                        Set<Integer> matchSet = matchList.get(i);
-                        for (int j : matchSet) {
-                            if (!finished[j]) {
-                                finished[j] = true;
-                                queue.offer(j);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        for (int i = 0; i < targetLength; i++) {
-            if (!finished[i])
-                return new int[0];
-        }
-        int stampSequenceLength = stack.size();
-        int[] stampSequence = new int[stampSequenceLength];
-        for (int i = 0; i < stampSequenceLength; i++)
-            stampSequence[i] = stack.pop();
-        return stampSequence;
+        for (i = 0; i < TC.length; i++) if (TC[i] != '*') return new int[]{};
+        int[] ans = new int[lans.size()];
+        for (i = 0; i < lans.size(); i++) ans[i] = lans.get(i);
+        return ans;
     }
 }
