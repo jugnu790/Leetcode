1550ba24ea33694c0fd0dc59743d6f64276c1ff5
DEEPESH UPADHYAY <87692640+jugnu790@users.noreply.github.com>
2022-08-21 23:53:08 +0530

    Time: 8 ms (77.03%), Space: 44.6 MB (85.36%) - LeetHub

---
+++ 6-zigzag-conversion/6-zigzag-conversion.java
@@ -0,0 +1,18 @@
+
+class Solution {
+    public String convert(String s, int numRows) {
+        if (numRows == 1) return s;
+        StringBuilder sb = new StringBuilder();
+        int n = s.length();
+        int cycleLen = 2 * numRows - 2;
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j + i < n; j += cycleLen) {
+                sb.append(s.charAt(j + i));
+                if (i != 0 && i != numRows - 1 && j + cycleLen - i < n) {
+                    sb.append(s.charAt(j + cycleLen - i));
+                }
+            }
+        }
+        return sb.toString();
+    }
+}
