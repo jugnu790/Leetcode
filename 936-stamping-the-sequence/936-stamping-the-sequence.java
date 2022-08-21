831314dc9bd8c683c2e47561e7063e5525914059
DEEPESH UPADHYAY <87692640+jugnu790@users.noreply.github.com>
2022-08-21 23:46:11 +0530

    Time: 97 ms (13.39%), Space: 65.7 MB (14.29%) - LeetHub

--- 936-stamping-the-sequence/936-stamping-the-sequence.java
+++ 936-stamping-the-sequence/936-stamping-the-sequence.java
@@ -1,46 +1,62 @@
 class Solution {
-  public int[] movesToStamp(String stamp, String target) {
-    List<Integer> ans = new ArrayList<>();
-    char[] T = target.toCharArray();
-    
-    boolean[] stamped = new boolean[target.length()];
-    int stampedCount = 0; 
-
-    while (stampedCount < T.length) {
-      boolean isStamped = false;
-      
-      for (int i = 0; i <= T.length - stamp.length(); ++i) {
-        if (stamped[i])
-          continue;
-        final int stampified = stampify(stamp, T, i);
-        if (stampified == 0)
-          continue;
-        stampedCount += stampified;
-        isStamped = true;
-        stamped[i] = true;
-        ans.add(i);
-      }
-      
-      if (!isStamped)
-        return new int[] {};
+    public int[] movesToStamp(String stamp, String target) {
+        int stampLength = stamp.length(), targetLength = target.length();
+        boolean[] finished = new boolean[targetLength];
+        Queue<Integer> queue = new LinkedList<Integer>();
+        Stack<Integer> stack = new Stack<Integer>();
+        List<Set<Integer>> matchList = new ArrayList<Set<Integer>>();
+        List<Set<Integer>> notMatchList = new ArrayList<Set<Integer>>();
+        int maxIndex = targetLength - stampLength;
+        for (int i = 0; i <= maxIndex; i++) {
+            Set<Integer> matchSet = new HashSet<Integer>();
+            Set<Integer> notMatchSet = new HashSet<Integer>();
+            for (int j = 0; j < stampLength; j++) {
+                if (stamp.charAt(j) == target.charAt(i + j))
+                    matchSet.add(i + j);
+                else
+                    notMatchSet.add(i + j);
+            }
+            matchList.add(matchSet);
+            notMatchList.add(notMatchSet);
+            if (notMatchSet.isEmpty()) {
+                stack.push(i);
+                for (int j = 0; j < stampLength; j++) {
+                    int index = i + j;
+                    if (!finished[index]) {
+                        finished[index] = true;
+                        queue.offer(index);
+                    }
+                }
+            }
+        }
+        while (!queue.isEmpty()) {
+            int index = queue.poll();
+            int startIndex = Math.max(0, index + 1 - stampLength);
+            int endIndex = Math.min(maxIndex, index);
+            for (int i = startIndex; i <= endIndex; i++) {
+                Set<Integer> notMatchSet = notMatchList.get(i);
+                if (notMatchSet.remove(index)) {
+                    if (notMatchSet.isEmpty()) {
+                        stack.push(i);
+                        Set<Integer> matchSet = matchList.get(i);
+                        for (int j : matchSet) {
+                            if (!finished[j]) {
+                                finished[j] = true;
+                                queue.offer(j);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < targetLength; i++) {
+            if (!finished[i])
+                return new int[0];
+        }
+        int stampSequenceLength = stack.size();
+        int[] stampSequence = new int[stampSequenceLength];
+        for (int i = 0; i < stampSequenceLength; i++)
+            stampSequence[i] = stack.pop();
+        return stampSequence;
     }
-
-    Collections.reverse(ans);
-    return ans.stream().mapToInt(i -> i).toArray();
-  }
-
-
-  private int stampify(final String stamp, char[] T, int s) {
-    int stampified = stamp.length();
-
-    for (int i = 0; i < stamp.length(); ++i)
-      if (T[s + i] == '*') 
-        --stampified;
-      else if (T[s + i] != stamp.charAt(i))
-        return 0;
-
-    Arrays.fill(T, s, s + stamp.length(), '*');
-
-    return stampified;
-  }
 }
