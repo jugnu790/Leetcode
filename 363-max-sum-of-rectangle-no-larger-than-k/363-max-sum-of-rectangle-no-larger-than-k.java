7b4bd3f7b87d8441131ce07e10f78c290749c061
DEEPESH UPADHYAY <87692640+jugnu790@users.noreply.github.com>
2022-08-27 23:43:22 +0530

    Time: 628 ms (57.14%), Space: 49.4 MB (75.54%) - LeetHub

--- 363-max-sum-of-rectangle-no-larger-than-k/363-max-sum-of-rectangle-no-larger-than-k.java
+++ 363-max-sum-of-rectangle-no-larger-than-k/363-max-sum-of-rectangle-no-larger-than-k.java
@@ -1,57 +1,27 @@
 class Solution {
-   public int maxSumSubmatrix(int[][] matrix, int k) {
-            if (matrix == null || matrix.length == 0) {
-                return 0;
+    public int maxSumSubmatrix(int[][] matrix, int k) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) { return 0; }
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int[][] sum = new int[row + 1][col + 1];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                sum[i + 1][j + 1] = sum[i + 1][j] + sum[i][j + 1] - sum[i][j] + matrix[i][j];
             }
-
-            int m = matrix.length;
-            int n = matrix[0].length;
-
-            int[][] preSum = new int[m][n + 1];
-
-            for (int i = 0; i < m; i++) {
-                for (int j = 1; j <= n; j++) {
-                    preSum[i][j] = preSum[i][j - 1] + matrix[i][j - 1];
-                }
-            }
-
-            int ans = Integer.MIN_VALUE;
-            for (int c1 = 1; c1 <= n; c1++) {
-                for (int c2 = c1; c2 <= n; c2++) {
-                    int[] preSumRow = new int[m];
-                    for (int row = 0; row < m; row++) {
-                        preSumRow[row] = preSum[row][c2] - preSum[row][c1 - 1];
+        }
+        int max = Integer.MIN_VALUE;
+        for (int i1 = 0; i1 < row; i1++) {
+            for (int j1 = 0; j1 < col; j1++) {
+                for (int i2 = i1; i2 < row; i2++) {
+                    for (int j2 = j1; j2 < col; j2++) {
+                        int ans = sum[i2 + 1][j2 + 1] + sum[i1][j1] - sum[i1][j2 + 1] - sum[i2 + 1][j1];
+                        if (ans <= k) {
+                            max = Math.max(max, ans);
+                        }
                     }
-
-                    ans = Math.max(ans, getMaxSubarrayHelper(preSumRow, k));
                 }
             }
-
-            return ans;
         }
-
-        private int getMaxSubarrayHelper(int[] nums, int target) {
-            TreeSet<Integer> treeSet = new TreeSet<>();
-
-            int ans = Integer.MIN_VALUE;
-            int[] preSum = new int[nums.length + 1];
-
-            for (int i = 1; i <= nums.length; i++) {
-                preSum[i] = preSum[i - 1] + nums[i - 1];
-            }
-
-            treeSet.add(0);
-            for (int i = 1; i <= nums.length; i++) {
-                Integer num = preSum[i] - target;
-                Integer ceiling = treeSet.ceiling(num);
-
-                if (ceiling != null) {
-                    ans = Math.max(ans, preSum[i] - ceiling);
-                }
-
-                treeSet.add(preSum[i]);
-            }
-
-            return ans;
-        }
+        return max;
     }
+}
