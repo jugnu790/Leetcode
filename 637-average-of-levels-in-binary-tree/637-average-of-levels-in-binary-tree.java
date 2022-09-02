4746cc60e538b1cbf635c614e6f10e68958024d6
DEEPESH UPADHYAY <87692640+jugnu790@users.noreply.github.com>
2022-09-02 15:32:37 +0530

    Time: 3 ms (76.99%), Space: 43.8 MB (91.84%) - LeetHub

--- 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.java
+++ 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.java
@@ -1,34 +1,32 @@
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
  *     int val;
  *     TreeNode left;
  *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ *     TreeNode(int x) { val = x; }
  * }
  */
-class Solution {
-    public List<Double> averageOfLevels(TreeNode root) {
-        List<Double> res = new ArrayList<>();
-        if(root == null) return res;
-        Queue<TreeNode> q = new LinkedList<>();
-        q.offer(root);
-        while(!q.isEmpty()){
-            int size = q.size();
-            double sum = 0;
-            for(int i = 0; i < size; i++){
-                TreeNode cur = q.poll();
-                sum += cur.val;
-                if(cur.left != null) q.offer(cur.left);
-                if(cur.right != null) q.offer(cur.right);
+public class Solution {
+    public List < Double > averageOfLevels(TreeNode root) {
+        List < Double > res = new ArrayList < > ();
+        Queue < TreeNode > queue = new LinkedList < > ();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            long sum = 0, count = 0;
+            Queue < TreeNode > temp = new LinkedList < > ();
+            while (!queue.isEmpty()) {
+                TreeNode n = queue.remove();
+                sum += n.val;
+                count++;
+                if (n.left != null)
+                    temp.add(n.left);
+                if (n.right != null)
+                    temp.add(n.right);
             }
-            res.add(sum / size);
+            queue = temp;
+            res.add(sum * 1.0 / count);
         }
         return res;
     }
