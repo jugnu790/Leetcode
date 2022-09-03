121a1756323f4b88420afc38c8b336f3fe8cb301
DEEPESH UPADHYAY <87692640+jugnu790@users.noreply.github.com>
2022-09-03 23:56:50 +0530

    Time: 1 ms (100.00%), Space: 42.1 MB (93.44%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.java
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.java
@@ -1,27 +1,27 @@
 class Solution {
+    List<Integer> ans = new ArrayList();
     public int[] numsSameConsecDiff(int N, int K) {
-        LinkedList<Integer> res = new LinkedList<>(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9));
-        for (int i = 1; i < N; i++) {
-            int size = res.size();
-            for (int j = 0; j < size; j++) {
-                int t = res.removeFirst();
-                if (t == 0) continue;
-                int mod = t % 10;
-                if (K == 0) {
-                    res.add(t * 10 + mod);
-                    continue;
-                }
-                if (mod - K >= 0) {
-                    res.add(t * 10 + mod - K);
-                }
-                if (mod + K < 10) {
-                    res.add(t * 10 + mod + K);
-                }
-            }
+        for(int i = 1;i<10;i++)
+            help(N,0,i,K);
+        if(N==1)
+            ans.add(0);
+        int [] res = new int [ans.size()];
+        for(int i = 0;i<ans.size();i++)
+            res[i] = ans.get(i);
+        return res;
+    }
+    private void help(int N, int number, int digit,int K)
+    {
+        if(N==1)
+        {
+            ans.add(number*10+digit);
+            return;
         }
-        int[] arr = new int[res.size()];
-        int i = 0;
-        while (!res.isEmpty()) {
-            arr[i++] = res.removeFirst();
-        }
-        return arr;}}
+        number*=10;
+        if(digit+K<10)
+            help(N-1,number+digit,digit+K,K);
+        if(digit-K>=0&&K!=0)
+            help(N-1,number+digit,digit-K,K);
+            
+    }
+}
