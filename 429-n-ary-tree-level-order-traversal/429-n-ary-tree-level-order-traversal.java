606151fb5b927139433f4afe126d2252cc81741f
DEEPESH UPADHYAY <87692640+jugnu790@users.noreply.github.com>
2022-09-05 13:31:47 +0530

    Time: 5 ms (43.54%), Space: 46.9 MB (74.58%) - LeetHub

--- 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.java
+++ 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.java
@@ -1,21 +1,44 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
 class Solution {
     public List<List<Integer>> levelOrder(Node root) {
-        List<List<Integer>> ans = new ArrayList<>();
+        List<List<Integer>> levelOrder = new ArrayList<List<Integer>>();
         if (root == null)
-            return ans;
-        Queue<Node> nodesQueue = new LinkedList<>();
-        nodesQueue.add(root);
-        while (!nodesQueue.isEmpty()) {
-            int size = nodesQueue.size();
-            List<Integer> level = new ArrayList<>();
-            while (size-- > 0) {
-                Node current = nodesQueue.remove();
-                for (Node child : current.children) 
-                    nodesQueue.add(child);
-                level.add(current.val);
+            return levelOrder;
+        Queue<Node> queue = new LinkedList<Node>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> curLevel = new ArrayList<Integer>();
+            for (int i = 0; i < size; i++) {
+                Node node = queue.poll();
+                curLevel.add(node.val);
+                List<Node> children = node.children;
+                if (children != null && children.size() > 0) {
+                    int childrenCount = children.size();
+                    for (int j = 0; j < childrenCount; j++)
+                        queue.offer(children.get(j));
+                }
             }
-            ans.add(level);
+            levelOrder.add(curLevel);
         }
-        return ans;
+        return levelOrder;
     }
 }
