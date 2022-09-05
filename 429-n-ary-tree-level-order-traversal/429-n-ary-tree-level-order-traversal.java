8ae31fcda4bc27f575add44084e0828efd70cd0b
DEEPESH UPADHYAY <87692640+jugnu790@users.noreply.github.com>
2022-09-05 13:30:13 +0530

    Time: 5 ms (43.54%), Space: 47.2 MB (61.94%) - LeetHub

--- 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.java
+++ 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.java
@@ -19,20 +19,26 @@ class Node {
 
 class Solution {
     public List<List<Integer>> levelOrder(Node root) {
-        List<List<Integer>> result = new ArrayList<>();
-        if (root == null) return result;
-        
-        List<Node> previousLayer = Arrays.asList(root);
-        while (!previousLayer.isEmpty()) {
-            List<Node> currentLayer = new ArrayList<>();
-            List<Integer> previousVals = new ArrayList<>();
-            for (Node node: previousLayer) {
-                previousVals.add(node.val);
-                currentLayer.addAll(node.children);
+        List<List<Integer>> levelOrder = new ArrayList<List<Integer>>();
+        if (root == null)
+            return levelOrder;
+        Queue<Node> queue = new LinkedList<Node>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> curLevel = new ArrayList<Integer>();
+            for (int i = 0; i < size; i++) {
+                Node node = queue.poll();
+                curLevel.add(node.val);
+                List<Node> children = node.children;
+                if (children != null && children.size() > 0) {
+                    int childrenCount = children.size();
+                    for (int j = 0; j < childrenCount; j++)
+                        queue.offer(children.get(j));
+                }
             }
-            result.add(previousVals);
-            previousLayer = currentLayer;
+            levelOrder.add(curLevel);
         }
-        return result;
+        return levelOrder;
     }
 }
