913ed347903d8407eb2ac789ed8b5bfe865f1224
DEEPESH UPADHYAY <87692640+jugnu790@users.noreply.github.com>
2022-09-05 13:30:58 +0530

    Time: 11 ms (7.02%), Space: 47.6 MB (30.43%) - LeetHub

--- 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.java
+++ 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.java
@@ -1,44 +1,22 @@
-/*
-// Definition for a Node.
-class Node {
-    public int val;
-    public List<Node> children;
-
-    public Node() {}
-
-    public Node(int _val) {
-        val = _val;
-    }
-
-    public Node(int _val, List<Node> _children) {
-        val = _val;
-        children = _children;
-    }
-};
-*/
-
 class Solution {
-    public List<List<Integer>> levelOrder(Node root) {
-        List<List<Integer>> levelOrder = new ArrayList<List<Integer>>();
-        if (root == null)
-            return levelOrder;
-        Queue<Node> queue = new LinkedList<Node>();
-        queue.offer(root);
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            List<Integer> curLevel = new ArrayList<Integer>();
-            for (int i = 0; i < size; i++) {
-                Node node = queue.poll();
-                curLevel.add(node.val);
-                List<Node> children = node.children;
-                if (children != null && children.size() > 0) {
-                    int childrenCount = children.size();
-                    for (int j = 0; j < childrenCount; j++)
-                        queue.offer(children.get(j));
-                }
-            }
-            levelOrder.add(curLevel);
-        }
-        return levelOrder;
+  public List<List<Integer>> levelOrder(Node root) {
+    if (root == null)
+      return new ArrayList<>();
+
+    List<List<Integer>> ans = new ArrayList<>();
+    Queue<Node> q = new ArrayDeque<>(Arrays.asList(root));
+
+    while (!q.isEmpty()) {
+      List<Integer> currLevel = new ArrayList<>();
+      for (int sz = q.size(); sz > 0; --sz) {
+        Node node = q.poll();
+        currLevel.add(node.val);
+        for (Node child : node.children)
+          q.offer(child);
+      }
+      ans.add(currLevel);
     }
+
+    return ans;
+  }
 }
