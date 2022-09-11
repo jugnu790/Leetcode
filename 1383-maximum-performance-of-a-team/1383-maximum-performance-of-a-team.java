b7230f2ff409242119fd474df14ea76df1db1e86
DEEPESH UPADHYAY <87692640+jugnu790@users.noreply.github.com>
2022-09-11 23:15:14 +0530

    Time: 89 ms (41.00%), Space: 69 MB (49.67%) - LeetHub

--- 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.java
+++ 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.java
@@ -1,22 +1,45 @@
 class Solution {
     public int maxPerformance(int n, int[] speed, int[] efficiency, int k) {
-        int[][] arr = new int[n][2];
+        final int MODULO = 1000000007;
+        int[][] speedEfficiencyArray = new int[n][2];
         for (int i = 0; i < n; i++) {
-            arr[i][0] = efficiency[i];
-            arr[i][1] = speed[i];
+            speedEfficiencyArray[i][0] = speed[i];
+            speedEfficiencyArray[i][1] = efficiency[i];
         }
-        Arrays.sort(arr, (a, b) -> b[0] - a[0]);
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        long sum = 0;
-        long res = 0;
-        for (int i = 0; i < n; i++) {
-            pq.add(arr[i][1]);
-            sum += arr[i][1];
-            if (pq.size() > k) {
-                sum -= pq.poll();
+        Arrays.sort(speedEfficiencyArray, new Comparator<int[]>() {
+            public int compare(int[] speedEfficiency1, int[] speedEfficiency2) {
+                if (speedEfficiency1[1] != speedEfficiency2[1])
+                    return speedEfficiency2[1] - speedEfficiency1[1];
+                else
+                    return speedEfficiency2[0] - speedEfficiency1[0];
             }
-            res = Math.max(res, sum * arr[i][0]);
+        });
+        long maxPerformance = 0;
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<Integer>();
+        long speedSum = 0;
+        int minEfficiency = Integer.MAX_VALUE;
+        for (int i = 0; i < k; i++) {
+            int[] speedEfficiency = speedEfficiencyArray[i];
+            int curSpeed = speedEfficiency[0];
+            int curEfficiency = speedEfficiency[1];
+            priorityQueue.offer(curSpeed);
+            speedSum += curSpeed;
+            minEfficiency = Math.min(minEfficiency, curEfficiency);
+            long curPerformance = speedSum * (long) minEfficiency;
+            maxPerformance = Math.max(maxPerformance, curPerformance);
         }
-        return (int) (res % 1000000007);      
+        for (int i = k; i < n; i++) {
+            int prevSpeed = priorityQueue.poll();
+            speedSum -= prevSpeed;
+            int[] speedEfficiency = speedEfficiencyArray[i];
+            int curSpeed = speedEfficiency[0];
+            int curEfficiency = speedEfficiency[1];
+            priorityQueue.offer(curSpeed);
+            speedSum += curSpeed;
+            minEfficiency = Math.min(minEfficiency, curEfficiency);
+            long curPerformance = speedSum * (long) minEfficiency;
+            maxPerformance = Math.max(maxPerformance, curPerformance);
+        }
+        return (int) (maxPerformance % MODULO);
     }
 }
