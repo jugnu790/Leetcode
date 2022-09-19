0fbe3b73d099356f9d70c41174ad2d2b1992174e
DEEPESH UPADHYAY <87692640+jugnu790@users.noreply.github.com>
2022-09-19 17:26:29 +0530

    Time: 51 ms (60.30%), Space: 70.6 MB (48.20%) - LeetHub

---
+++ 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<List<String>> findDuplicate(String[] paths) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String path : paths) {
+            String[] dirAndFiles = path.split(" ");
+            for (int i = 1; i < dirAndFiles.length; i++) {
+                String content = dirAndFiles[i].substring(dirAndFiles[i].indexOf("(") + 1, dirAndFiles[i].indexOf(")"));
+                if (!map.containsKey(content)) {
+                    map.put(content, new ArrayList<>());
+                }
+                List<String> dirs = map.get(content);
+                dirs.add(dirAndFiles[0] + "/" + dirAndFiles[i].substring(0, dirAndFiles[i].indexOf("(")));
+                map.put(content, dirs);
+            }
+        }
+
+        List<List<String>> result = new ArrayList<>();
+        for (String content : map.keySet()) {
+            if (map.get(content).size() > 1) {
+                List<String> dupFile = new ArrayList<>();
+                for (String dir : map.get(content)) {
+                    dupFile.add(dir);
+                }
+                result.add(dupFile);
+            }
+        }
+        return result;
+    }
+}
