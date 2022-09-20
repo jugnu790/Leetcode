d90870eec3d28ee0b75ede9b6f679afa332ce8ce
DEEPESH UPADHYAY <87692640+jugnu790@users.noreply.github.com>
2022-09-20 22:21:30 +0530

    Time: 72 ms (68.03%), Space: 78 MB (54.11%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.java
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.java
@@ -1,21 +1,16 @@
 class Solution {
-  public int findLength(int[] A, int[] B) {
-    int m = A.length;
-    int n = B.length;
-    int[][] p = new int[m][n];
-    int max = 0;
-    for (int i = 0; i < m; i++) {
-      for (int j = 0; j < n; j++) {
-        if(A[i] == B[j]) {
-          if(i == 0 || j == 0) {
-            p[i][j] = 1;
-          } else {
-            p[i][j] = p[i - 1][j - 1] + 1;
-          }
+    public int findLength(int[] A, int[] B) {
+        int m = A.length, n = B.length;
+        int[][] dp = new int[m + 1][n + 1];
+        int ans = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (A[i] == B[j]) {
+                    dp[i + 1][j + 1] = dp[i][j] + 1;
+                    ans = Math.max(ans, dp[i + 1][j + 1]);
+                }
+            }
         }
-        max = Math.max(p[i][j], max);
-      }
+        return ans;
     }
-    return max;
-  }
 }
