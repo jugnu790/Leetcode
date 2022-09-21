9c5692a279105b385fd5aad157a69cf1d4091fee
DEEPESH UPADHYAY <87692640+jugnu790@users.noreply.github.com>
2022-09-21 17:05:23 +0530

    Time: 23 ms (5.47%), Space: 72.1 MB (47.27%) - LeetHub

--- 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.java
+++ 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.java
@@ -1,24 +1,32 @@
 class Solution {
-    public int[] sumEvenAfterQueries(int[] A, int[][] queries) {
-        int[] result = new int[queries.length];
+     public int[] sumEvenAfterQueries(int[] A, int[][] queries) {
+        int[] res = new int[queries.length];
+        
+        // Get initial even summation
         int sum = 0;
-        for (int i = 0; i < A.length; i++) {
-            if (A[i] % 2 == 0) {
-                sum += A[i];
-            }
-        }
+        for (int i = 0; i < A.length; i++) sum += (A[i] % 2 == 0) ?  A[i] : 0;
+        
         for (int i = 0; i < queries.length; i++) {
             int index = queries[i][1];
             int value = queries[i][0];
-            if (A[index] % 2 == 0) {
-                sum -= A[index];
-            }
+            int previousValue = A[index];
+            
+            boolean wasEven = false;
+            boolean nowEven = false;
+            
+            if (A[index] % 2 == 0) wasEven = true;
             A[index] += value;
-            if (A[index] % 2 == 0) {
+            if (A[index] % 2 == 0) nowEven = true;
+            
+            if (wasEven && nowEven) { // was even before and after
+                sum += value;
+            } else if (wasEven && !nowEven) { // was even and now odd
+                sum -= previousValue;
+            } else if (!wasEven && nowEven) { // was odd and became even
                 sum += A[index];
-            }
-            result[i] = sum;
+            } else if (!wasEven && !nowEven) {} // was odd then and still odd now, do nothing
+            res[i] = sum;  
         }
-        return result;
+        return res;
     }
 }
