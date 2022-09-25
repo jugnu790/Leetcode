f93972bfe03737c129c6b09c0b0bcc530032c6b2
DEEPESH UPADHYAY <87692640+jugnu790@users.noreply.github.com>
2022-09-25 14:10:20 +0530

    Time: 4 ms (91.93%), Space: 42.5 MB (97.53%) - LeetHub

--- 622-design-circular-queue/622-design-circular-queue.java
+++ 622-design-circular-queue/622-design-circular-queue.java
@@ -1,56 +1,55 @@
 class MyCircularQueue {
-    int [] arr;
-    int size;
-    int capacity;
-    int front;
-    int back;
-    /** Initialize your data structure here. Set the size of the queue to be k. */
-    public MyCircularQueue(int k) {
-        arr = new int[k];
-        capacity=k;
-        size=0;
-        front=0;
-        back = -1;
-    }
-    
-    /** Insert an element into the circular queue. Return true if the operation is successful. */
-    public boolean enQueue(int value) {
-        if(size==capacity){
-            return false;
-        }
-        ++back;
-        arr[back%arr.length]=value;
-        ++size;
-        return true;
-    }
-    
-    /** Delete an element from the circular queue. Return true if the operation is successful. */
-    public boolean deQueue() {
-        if(size==0)return false;
-        ++front;
-        --size;
-        return true;
-    }
-    
-    /** Get the front item from the queue. */
-    public int Front() {
-        if(size==0)return -1;
-        return arr[front%arr.length];
-    }
-    
-    /** Get the last item from the queue. */
-    public int Rear() {
-        if(size==0)return-1;
-        return arr[back%arr.length];
-    }
-    
-    /** Checks whether the circular queue is empty or not. */
-    public boolean isEmpty() {
-        return size==0;
-    }
-    
-    /** Checks whether the circular queue is full or not. */
-    public boolean isFull() {
-        return size==capacity;
-    }
+  /** Initialize your data structure here. Set the size of the queue to be k. */
+  public MyCircularQueue(int k) {
+    this.k = k;
+    this.q = new int[k];
+    this.rear = k - 1;
+  }
+
+  /** Insert an element into the circular queue. Return true if the operation is successful. */
+  public boolean enQueue(int value) {
+    if (isFull())
+      return false;
+
+    rear = ++rear % k;
+    q[rear] = value;
+    ++size;
+    return true;
+  }
+
+  /** Delete an element from the circular queue. Return true if the operation is successful. */
+  public boolean deQueue() {
+    if (isEmpty())
+      return false;
+
+    front = ++front % k;
+    --size;
+    return true;
+  }
+
+  /** Get the front item from the queue. */
+  public int Front() {
+    return isEmpty() ? -1 : q[front];
+  }
+
+  /** Get the last item from the queue. */
+  public int Rear() {
+    return isEmpty() ? -1 : q[rear];
+  }
+
+  /** Checks whether the circular queue is empty or not. */
+  public boolean isEmpty() {
+    return size == 0;
+  }
+
+  /** Checks whether the circular queue is full or not. */
+  public boolean isFull() {
+    return size == k;
+  }
+
+  private final int k;
+  private int[] q;
+  private int size = 0;
+  private int front = 0;
+  private int rear;
 }
