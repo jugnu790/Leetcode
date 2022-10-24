75537f4f1ca9047166f824c123e0967500b4ef48
DEEPESH UPADHYAY <87692640+jugnu790@users.noreply.github.com>
2022-10-24 20:43:25 +0530

    Time: 57 ms (44.38%), Space: 57.9 MB (37.23%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.java
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.java
@@ -1,22 +1,36 @@
 class Solution {
     public int maxLength(List<String> arr) {
-       int[] dp = new int[1];
-        for (String s : arr) {
-            int mask = 0, dup = 0;
-            for (char c : s.toCharArray()) {
-                dup |= mask & 1 << (c - 'a');
-                mask |= 1 << (c - 'a');
-            }
-            if (dup > 0) continue;
-            for (int i = dp.length - 1; i >= 0; --i) {
-                if ((dp[i] & mask) == 0) {
-                    dp = Arrays.copyOf(dp, dp.length + 1);
-                    dp[dp.length - 1] = dp[i] | mask;
+        List<String> list = new ArrayList<String>();
+        list.add("");
+        for (int i = 0; i < arr.size(); i++) {
+            String str = arr.get(i);
+            if (isUnique(str)) {
+                int size = list.size();
+                for (int j = 0; j < size; j++) {
+                    String temp = list.get(j) + str;
+                    if (isUnique(temp)) {
+                        list.add(temp);
+                    }
                 }
             }
         }
-        int res = 0;
-        for (int x : dp) res = Math.max(res, Integer.bitCount(x));
-        return res;
+        int max = 0;
+        for (int i = 0; i < list.size(); i++) {
+            max = Math.max(max, list.get(i).length());
+        }
+        return max;
+    }
+
+    public static boolean isUnique(String str) {
+        int[] arr = new int[26];
+        for (int i = 0; i < str.length(); i++) {
+            arr[str.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > 1) {
+                return false;
+            }
+        }
+        return true;
     }
 }
