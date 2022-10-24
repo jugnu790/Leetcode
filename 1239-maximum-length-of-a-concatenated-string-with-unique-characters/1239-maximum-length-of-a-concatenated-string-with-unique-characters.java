5b7311b0553b377a45ddb6b267c3e03103976544
DEEPESH UPADHYAY <87692640+jugnu790@users.noreply.github.com>
2022-10-24 20:44:56 +0530

    Time: 37 ms (58.29%), Space: 43.7 MB (76.12%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.java
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.java
@@ -1,36 +1,22 @@
 class Solution {
     public int maxLength(List<String> arr) {
-        List<String> list = new ArrayList<String>();
-        list.add("");
-        for (int i = 0; i < arr.size(); i++) {
-            String str = arr.get(i);
-            if (isUnique(str)) {
-                int size = list.size();
-                for (int j = 0; j < size; j++) {
-                    String temp = list.get(j) + str;
-                    if (isUnique(temp)) {
-                        list.add(temp);
-                    }
-                }
-            }
-        }
+        List<Integer> mask = new ArrayList<>();
         int max = 0;
-        for (int i = 0; i < list.size(); i++) {
-            max = Math.max(max, list.get(i).length());
+        mask.add(0);
+        for (String s : arr) {
+            int dup = 0, cur = 0;
+            for (char c : s.toCharArray()) {
+                dup |= cur & (1 << (c - 'a'));
+                cur |= 1 << (c - 'a');
+            }
+            if (dup > 0) continue;
+            for (int i = 0; i < mask.size(); ++i) {
+                if ((mask.get(i) & cur) > 0) continue;
+                int comb = mask.get(i) | cur;
+                mask.add(comb);
+                max = Math.max(max, Integer.bitCount(comb));
+            }
         }
         return max;
-    }
-
-    public static boolean isUnique(String str) {
-        int[] arr = new int[26];
-        for (int i = 0; i < str.length(); i++) {
-            arr[str.charAt(i) - 'a']++;
-        }
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > 1) {
-                return false;
-            }
-        }
-        return true;
     }
 }
