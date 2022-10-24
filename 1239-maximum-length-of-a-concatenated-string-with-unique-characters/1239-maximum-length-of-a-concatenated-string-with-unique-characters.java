121319ab6902164744d835aafaee2e5b3ca52f1d
DEEPESH UPADHYAY <87692640+jugnu790@users.noreply.github.com>
2022-10-24 20:42:05 +0530

    Time: 2251 ms (5.02%), Space: 629.3 MB (5.02%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.java
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.java
@@ -4,8 +4,8 @@ class Solution {
         for (String s : arr) {
             int mask = 0, dup = 0;
             for (char c : s.toCharArray()) {
-                dup |= mask & 1 << c - 'a';
-                mask |= 1 << c - 'a';
+                dup |= mask & 1 << (c - 'a');
+                mask |= 1 << (c - 'a');
             }
             if (dup > 0) continue;
             for (int i = dp.length - 1; i >= 0; --i) {
