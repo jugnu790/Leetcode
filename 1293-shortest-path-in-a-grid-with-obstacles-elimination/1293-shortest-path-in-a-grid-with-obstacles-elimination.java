80438aa60f19472192752132fde960d0b8efa524
DEEPESH UPADHYAY <87692640+jugnu790@users.noreply.github.com>
2022-10-30 21:12:07 +0530

    Time: 58 ms (51.50%), Space: 55.4 MB (31.04%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
@@ -2,44 +2,37 @@ class Solution {
     public int shortestPath(int[][] grid, int k) {
         int m = grid.length;
         int n = grid[0].length;
-        int[][][] visited = new int[m][n][k+1];
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                for(int l = 0; l <= k; l++){
-                    visited[i][j][l] = Integer.MAX_VALUE;
+        int[][] dir = new int[][]{{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+        boolean[][][] visited = new boolean[m][n][k + 1];
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{0, 0, k});
+        visited[0][0][k] = true;
+        int step = 0;
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                int[] curr = q.poll();
+                int x = curr[0];
+                int y = curr[1];
+                int rem = curr[2];
+                if (x == m - 1 && y == n - 1) {
+                    return step;
                 }
-            }
-        }
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{0,0,k});
-        visited[0][0][k] = 0;
-        int[][] dirs = {{0,1},{0,-1},{1,0},{-1,0}};
-        while(!queue.isEmpty()){
-            int[] cur = queue.poll();
-            int x = cur[0];
-            int y = cur[1];
-            int remain = cur[2];
-            if(x == m-1 && y == n-1){
-                return visited[x][y][remain];
-            }
-            for(int[] dir : dirs){
-                int nx = x + dir[0];
-                int ny = y + dir[1];
-                if(nx < 0 || nx >= m || ny < 0 || ny >= n){
-                    continue;
-                }
-                if(grid[nx][ny] == 1){
-                    if(remain > 0 && visited[nx][ny][remain-1] > visited[x][y][remain] + 1){
-                        visited[nx][ny][remain-1] = visited[x][y][remain] + 1;
-                        queue.add(new int[]{nx,ny,remain-1});
+                for (int[] d : dir) {
+                    int nextX = x + d[0];
+                    int nextY = y + d[1];
+                    if (nextX < 0 || nextX >= m || nextY < 0 || nextY >= n) {
+                        continue;
                     }
-                }else{
-                    if(visited[nx][ny][remain] > visited[x][y][remain] + 1){
-                        visited[nx][ny][remain] = visited[x][y][remain] + 1;
-                        queue.add(new int[]{nx,ny,remain});
+                    int nextRem = rem - grid[nextX][nextY];
+                    if (nextRem < 0 || visited[nextX][nextY][nextRem]) {
+                        continue;
                     }
+                    q.offer(new int[]{nextX, nextY, nextRem});
+                    visited[nextX][nextY][nextRem] = true;
                 }
             }
+            step++;
         }
         return -1;
     }
