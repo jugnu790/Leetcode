3820a711508a96f7b351dcd504e11d0228453fc9
DEEPESH UPADHYAY <87692640+jugnu790@users.noreply.github.com>
2023-04-08 15:53:09 +0530

    Time: 24 ms (99.84%) | Memory: 42.6 MB (62.89%) - LeetSync

--- clone-graph/clone-graph.java
+++ clone-graph/clone-graph.java
@@ -1,19 +1,44 @@
-
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
 
 class Solution {
-    Node[] copyNodes = new Node[100];
-    
-    public Node cloneGraph(Node node) 
-    {
-        if (node == null) return null; // stupid null nodes
-        
-        int ind = node.val-1; // So I don't have to write "node.val-1" all the time
-        if (copyNodes[ind] == null) 
-        {
-            copyNodes[ind] = new Node(node.val); 
-            for (Node neighbor : node.neighbors) 
-                copyNodes[ind].neighbors.add(cloneGraph(neighbor));
+    public Node cloneGraph(Node node) {
+        if (node == null) {
+            return null;
         }
-        return copyNodes[ind]; // Just return the copied node if it exists.
+        
+        Map<Node, Node> visited = new HashMap<>();
+        return cloneGraphHelper(node, visited);
+    }
+    
+    private Node cloneGraphHelper(Node node, Map<Node, Node> visited) {
+        Node copy = new Node(node.val);
+        visited.put(node, copy);
+        for (Node neighbor : node.neighbors) {
+            if (visited.containsKey(neighbor)) {
+                copy.neighbors.add(visited.get(neighbor));
+            } else {
+                Node neighborCopy = cloneGraphHelper(neighbor, visited);
+                copy.neighbors.add(neighborCopy);
+            }
+        }
+        return copy;
     }
 }
