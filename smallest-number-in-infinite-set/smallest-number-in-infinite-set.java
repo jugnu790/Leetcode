f7cd23860034708d7124167f36f39aaea1613478
DEEPESH UPADHYAY <87692640+jugnu790@users.noreply.github.com>
2023-04-25 14:52:49 +0530

    Time: 11 ms (88.39%) | Memory: 43.1 MB (66.01%) - LeetSync

---
+++ smallest-number-in-infinite-set/smallest-number-in-infinite-set.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class SmallestInfiniteSet {
+    private int minNum = 1;
+    private PriorityQueue<Integer> heap = new PriorityQueue<>();
+
+    public int popSmallest() {
+        if (!heap.isEmpty()) {
+            return heap.poll();
+        }
+        minNum++;
+        return minNum - 1;
+    }
+
+    public void addBack(int num) {
+        if (minNum > num && !heap.contains(num)) {
+            heap.offer(num);
+        }
+    }
+}
